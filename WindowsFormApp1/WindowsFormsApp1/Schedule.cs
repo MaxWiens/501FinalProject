efca3f93bd3cc80f54da9d3b6a8a154cabb9d118
Max Wiens-Evangelista <rothmoss@gmail.com>
2018-12-05 20:24:57 -0600

    Added most of the classes

---
+++ WindowsFormApp1/WindowsFormsApp1/Schedule.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SVTApp
+{
+    class Schedule
+    {
+        public int SectionNumber { get; private set; }
+        string Consent { get; }
+    }
+}
